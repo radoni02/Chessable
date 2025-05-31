ba47b2d27a9a20d6a9c8d00241f3e01f66ed0e2a
Radosław <radekkiczekk@gmail.com>
2025-05-31 13:22:08 +0200

    refactored king possible moves

--- Chess/Figures/King.cs
+++ Chess/Figures/King.cs
@@ -35,60 +35,80 @@ public class King : Figure
     private HashSet<string> KingPossibleMoves(Checkerboard checkerboard, Field currentField)
     {
         var forbiddenFieldsForKing = checkerboard.Board.SelectMany(fl => fl)
-                                                            .Where(f => f.IsUsed
-                                                                    && f.Figure.IsWhite != currentField.Figure.IsWhite)
+                                                        .Where(f => f.IsUsed && f.Figure?.IsWhite != currentField.Figure.IsWhite)
                                                             .Select(field => field.Figure)
                                                             .SelectMany(s => s.AttackedFields)
                                                             .Distinct();
 
-        if(currentField.Figure.MoveConut == 0)
+        var possibleMoves = GetBasicKingMoves(checkerboard, currentField, forbiddenFieldsForKing);
+
+        if (currentField.Figure.MoveConut == 0)
         {
-            var possibleCastling = checkerboard.Board.SelectMany(fl => fl)
-                                                        .Where(field => field.Figure.Name.Equals("Rook")
-                                                            && field.Figure.IsWhite == currentField.Figure.IsWhite
-                                                            && field.Figure.MoveConut == 0);
-            //need to check if all field between is empty and also if it is not attacked
+            var castlingMoves = GetCastlingMoves(checkerboard, currentField, forbiddenFieldsForKing);
+            foreach (var move in castlingMoves)
+            {
+                possibleMoves.Add(move);
+        }
         }
 
+        return possibleMoves;
         
+        HashSet<string> GetBasicKingMoves(Checkerboard board, Field current, IEnumerable<Field> forbidden)
+        {
+            return board.Board.SelectMany(fl => fl)
+                              .Where(field => PotentialFieldIsInKingMoveRange(field, current)
+                                           && ((field.IsUsed && field.Figure?.IsWhite != current.Figure.IsWhite) || !field.IsUsed))
+                              .Where(field => !forbidden.Any(ff => ff.Row == field.Row && ff.Col == field.Col))
+                              .Select(field => $"{field.Row - 1}{field.Col - 1}")
+                              .ToHashSet();
+        }
 
-        return checkerboard.Board.SelectMany(fl => fl)
-                                    .Select(field =>
+        HashSet<string> GetCastlingMoves(Checkerboard board, Field current, IEnumerable<Field> forbidden)
                                     {
-                                        var lissss = new HashSet<Field>();
-                                        if (PotentialFieldIsInKingMoveRange(field, currentField)
-                                        && ((field.IsUsed && field.Figure.IsWhite != currentField.Figure.IsWhite) || !field.IsUsed))
-                                        {
-                                            lissss.Add(field);
-                                        }
-                                        return lissss;
-                                    })
-                                    .SelectMany(elf => elf)
-                                    .Select(field =>
-                                    {
-                                        if(forbiddenFieldsForKing.Any(ff => ff.Row == field.Row && ff.Col == field.Col))
-                                        {
-                                            return string.Empty;
-                                        }
-                                        return $"{field.Row - 1}{field.Col - 1}";
+            var castlingMoves = new HashSet<string>();
+            var possibleCastling = board.Board.SelectMany(fl => fl)
+                                             .Where(field => field.Figure?.Name == "Rook"
+                                                          && field.Figure.IsWhite == current.Figure.IsWhite
+                                                          && field.Figure.MoveConut == 0);
 
-                                    })
-                                    .Where(value => !value.Equals(string.Empty))
-                                    .ToHashSet();
+            foreach (var rook in possibleCastling)
+                                        {
+                if (IsCastlingValid(board, current, rook, forbidden))
+                {
+                    int castlingCol = rook.Col < current.Col ? current.Col - 2 : current.Col + 2;
+                    castlingMoves.Add($"{current.Row - 1}{castlingCol - 1}");
+                                        }
+            }
+            return castlingMoves;
+        }
+
+        bool IsCastlingValid(Checkerboard board, Field king, Field rook, IEnumerable<Field> forbidden)
+                                    {
+            var minCol = Math.Min(king.Col, rook.Col);
+            var maxCol = Math.Max(king.Col, rook.Col);
+
+            var fieldsInBetween = board.Board.SelectMany(fl => fl)
+                                            .Where(field => field.Row == king.Row
+                                                         && field.Col > minCol
+                                                         && field.Col < maxCol)
+                                            .ToList();
+
+            bool allFieldsEmpty = fieldsInBetween.All(field => !field.IsUsed);
+
+            var kingPath = new[] { king.Col, king.Col + (rook.Col > king.Col ? 1 : -1), king.Col + (rook.Col > king.Col ? 2 : -2) };
+            bool pathUnderAttack = kingPath.Any(col =>
+                forbidden.Any(ff => ff.Row == king.Row && ff.Col == col));
+
+            return allFieldsEmpty && !pathUnderAttack;
+                                        }
     }
 
 
     private bool PotentialFieldIsInKingMoveRange(Field potentialField, Field currentField)
     {
-        if ((potentialField.Row == currentField.Row
-            || potentialField.Row == currentField.Row - 1
-            || potentialField.Row == currentField.Row + 1)
-            && (potentialField.Col == currentField.Col
-            || potentialField.Col == currentField.Col - 1
-            || potentialField.Col == currentField.Col + 1))
-        {
+        if (Math.Abs(currentField.Row - potentialField.Row) <= 1 && Math.Abs(currentField.Col - potentialField.Col) <= 1
+                   && !(potentialField.Row == currentField.Row && potentialField.Col == currentField.Col))
             return true;
-        }
         return false;
     }
 }
