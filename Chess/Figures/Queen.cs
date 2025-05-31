b7cd31f425a1d14b6903974c379ec1aa89742e91
Radosław <radoslaw.kiczek@comarch.pl>
2025-05-31 09:59:02 +0200

    added queen

---
+++ Chess/Figures/Queen.cs
@@ -0,0 +1,55 @@
+﻿using Chess.Chessboard;
+using Chess.Figures.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Figures
+{
+    public class Queen : Figure
+    {
+        private StraightFigureMovment StraightFigureMovment { get; } = new StraightFigureMovment();
+        private DiagonallyFigureMovment DiagonallyFigureMovment { get; } = new DiagonallyFigureMovment();
+        public Queen(bool isWhite, int value, string name) : base(isWhite, value, name)
+        {
+        }
+
+        public override void CalculateAtackedFields(Checkerboard checkerboard, Field currentField)
+        {
+            var selectedFields = new List<Field>();
+
+            selectedFields = StraightFigureMovment.GetRowFields(checkerboard, currentField, true);
+            selectedFields.AddRange(StraightFigureMovment.GetRowFields(checkerboard, currentField, false));
+            selectedFields.AddRange(StraightFigureMovment.GetColFields(checkerboard, currentField, true));
+            selectedFields.AddRange(StraightFigureMovment.GetColFields(checkerboard, currentField, false));
+
+            selectedFields.AddRange(DiagonallyFigureMovment.SelectValidFieldsOnTheDiagonals(checkerboard, currentField, 1, 1));
+            selectedFields.AddRange(DiagonallyFigureMovment.SelectValidFieldsOnTheDiagonals(checkerboard, currentField, 1, -1));
+            selectedFields.AddRange(DiagonallyFigureMovment.SelectValidFieldsOnTheDiagonals(checkerboard, currentField, -1, 1));
+            selectedFields.AddRange(DiagonallyFigureMovment.SelectValidFieldsOnTheDiagonals(checkerboard, currentField, -1, -1));
+
+            AttackedFields = selectedFields;
+        }
+
+        public override HashSet<string> PossibleMoves(Checkerboard checkerboard, Field currentField)
+        {
+            var selectedFields = new List<Field>();
+
+            selectedFields = StraightFigureMovment.GetRowFields(checkerboard, currentField, true);
+            selectedFields.AddRange(StraightFigureMovment.GetRowFields(checkerboard, currentField, false));
+            selectedFields.AddRange(StraightFigureMovment.GetColFields(checkerboard, currentField, true));
+            selectedFields.AddRange(StraightFigureMovment.GetColFields(checkerboard, currentField, false));
+
+            selectedFields.AddRange( DiagonallyFigureMovment.SelectValidFieldsOnTheDiagonals(checkerboard, currentField, 1, 1));
+            selectedFields.AddRange(DiagonallyFigureMovment.SelectValidFieldsOnTheDiagonals(checkerboard, currentField, 1, -1));
+            selectedFields.AddRange(DiagonallyFigureMovment.SelectValidFieldsOnTheDiagonals(checkerboard, currentField, -1, 1));
+            selectedFields.AddRange(DiagonallyFigureMovment.SelectValidFieldsOnTheDiagonals(checkerboard, currentField, -1, -1));
+
+            return selectedFields
+                        .Select(field => $"{field.Row - 1}{field.Col - 1}")
+                        .ToHashSet();
+        }
+    }
+}
