0565df9f3a625ea624fc320d14bd480de5657064
Radosław <radekkiczekk@gmail.com>
2025-06-19 15:42:09 +0200

    More efficient building of default position

--- Chess.Tests/SetupTests/ChessboardPositions.cs
+++ Chess.Tests/SetupTests/ChessboardPositions.cs
@@ -10,94 +10,50 @@ namespace Chess.Tests.SetupTests
 {
     internal class ChessboardPositions
     {
+
         public Chessboard.Checkerboard GetDefaultPosition()
         {
-            var chessboard = new Chessboard.Checkerboard();
-            chessboard.Board = new List<List<Field>>() {
-            new List<Field>() {
-                new Field(true, new Rook(true, 5, "Rook"), 1, 1),
-                new Field(true, new Knight(true, 3, "Knight"), 1, 2),
-                new Field(true, new Bishop(true, 3, "Bishop"), 1, 3),
-                new Field(true, new Queen(true, 10, "Queen"), 1, 4),
-                new Field(true, new King(true, 1000, "King"), 1, 5),
-                new Field(true, new Bishop(true, 3, "Bishop"), 1, 6),
-                new Field(true, new Knight(true, 3, "Knight"), 1, 7),
-                new Field(true, new Rook(true, 5, "Rook"), 1, 8),
-            },
-            new List<Field>() {
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 1),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 2),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 3),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 4),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 5),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 6),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 7),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 8),
-            },
-            new List<Field>() {
-                new Field(false, 3, 1),
-                new Field(false, 3, 2),
-                new Field(false, 3, 3),
-                new Field(false, 3, 4),
-                new Field(false, 3, 5),
-                new Field(false, 3, 6),
-                new Field(false, 3, 7),
-                new Field(false, 3, 8),
+            var chessboard = GetEmptyBoard();
 
-            },
-            new List<Field>() {
-                new Field(false, 4, 1),
-                new Field(false, 4, 2),
-                new Field(false, 4, 3),
-                new Field(false, 4, 4),
-                new Field(false, 4, 5),
-                new Field(false, 4, 6),
-                new Field(false, 4, 7),
-                new Field(false, 4, 8),
+            chessboard.Board[0][0] = new Field(true, new Rook(true, 5, "Rook"), 1, 1);
+            chessboard.Board[0][1] = new Field(true, new Knight(true, 3, "Knight"), 1, 2);
+            chessboard.Board[0][2] = new Field(true, new Bishop(true, 3, "Bishop"), 1, 3);
+            chessboard.Board[0][3] = new Field(true, new Queen(true, 10, "Queen"), 1, 4);
+            chessboard.Board[0][4] = new Field(true, new King(true, 1000, "King"), 1, 5);
+            chessboard.Board[0][5] = new Field(true, new Bishop(true, 3, "Bishop"), 1, 6);
+            chessboard.Board[0][6] = new Field(true, new Knight(true, 3, "Knight"), 1, 7);
+            chessboard.Board[0][7] = new Field(true, new Rook(true, 5, "Rook"), 1, 8);
 
-            },
-            new List<Field>() {
-                new Field(false, 5, 1),
-                new Field(false, 5, 2),
-                new Field(false, 5, 3),
-                new Field(false, 5, 4),
-                new Field(false, 5, 5),
-                new Field(false, 5, 6),
-                new Field(false, 5, 7),
-                new Field(false, 5, 8),
-            },
-            new List<Field>() {
-                new Field(false, 6, 1),
-                new Field(false, 6, 2),
-                new Field(false, 6, 3),
-                new Field(false, 6, 4),
-                new Field(false, 6, 5),
-                new Field(false, 6, 6),
-                new Field(false, 6, 7),
-                new Field(false, 6, 8),
-            },
-            new List<Field>() {
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 1),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 2),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 3),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 4),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 5),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 6),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 7),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 8),
-            },
-            new List<Field>() {
-                new Field(true, new Rook(false, 5, "Rook"), 8, 1),
-                new Field(true, new Knight(false, 3, "Knight"), 8, 2),
-                new Field(true, new Bishop(false, 3, "Bishop"), 8, 3),
-                new Field(true, new Queen(false, 10, "Queen"), 8, 4),
-                new Field(true, new King(false, 1000, "King"), 8, 5),
-                new Field(true, new Bishop(false, 3, "Bishop"), 8, 6),
-                new Field(true, new Knight(false, 3, "Knight"), 8, 7),
-                new Field(true, new Rook(false, 5, "Rook"), 8, 8),
-            } }; 
+            chessboard.Board[1][0] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 1);
+            chessboard.Board[1][1] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 2);
+            chessboard.Board[1][2] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 3);
+            chessboard.Board[1][3] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 4);
+            chessboard.Board[1][4] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 5);
+            chessboard.Board[1][5] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 6);
+            chessboard.Board[1][6] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 7);
+            chessboard.Board[1][7] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 8);
+
+            chessboard.Board[6][0] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 1);
+            chessboard.Board[6][1] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 2);
+            chessboard.Board[6][2] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 3);
+            chessboard.Board[6][3] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 4);
+            chessboard.Board[6][4] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 5);
+            chessboard.Board[6][5] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 6);
+            chessboard.Board[6][6] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 7);
+            chessboard.Board[6][7] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 8);
+
+            chessboard.Board[7][0] = new Field(true, new Rook(false, 5, "Rook"), 8, 1);
+            chessboard.Board[7][1] = new Field(true, new Knight(false, 3, "Knight"), 8, 2);
+            chessboard.Board[7][2] = new Field(true, new Bishop(false, 3, "Bishop"), 8, 3);
+            chessboard.Board[7][3] = new Field(true, new Queen(false, 10, "Queen"), 8, 4);
+            chessboard.Board[7][4] = new Field(true, new King(false, 1000, "King"), 8, 5);
+            chessboard.Board[7][5] = new Field(true, new Bishop(false, 3, "Bishop"), 8, 6);
+            chessboard.Board[7][6] = new Field(true, new Knight(false, 3, "Knight"), 8, 7);
+            chessboard.Board[7][7] = new Field(true, new Rook(false, 5, "Rook"), 8, 8);
+
             return chessboard;
         }
+
         public Checkerboard SetPawnWithNonPossibleMoves()
         {
             var chessboard = GetDefaultPosition();
@@ -118,18 +74,7 @@ namespace Chess.Tests.SetupTests
 
         public Checkerboard GetMidGamePosition()
         {
-            var chessboard = new Checkerboard();
-            chessboard.Board = new List<List<Field>>();
-
-            for (int row = 1; row <= 8; row++)
-            {
-                var boardRow = new List<Field>();
-                for (int col = 1; col <= 8; col++)
-                {
-                    boardRow.Add(new Field(false, row, col));
-                }
-                chessboard.Board.Add(boardRow);
-            }
+            var chessboard = GetEmptyBoard();
 
             // White pieces
             chessboard.Board[0][4] = new Field(true, new King(true, 1000, "King"), 1, 5); // e1
@@ -156,18 +101,7 @@ namespace Chess.Tests.SetupTests
 
         public Checkerboard GetTacticalPosition()
         {
-            var chessboard = new Checkerboard();
-            chessboard.Board = new List<List<Field>>();
-
-            for (int row = 1; row <= 8; row++)
-            {
-                var boardRow = new List<Field>();
-                for (int col = 1; col <= 8; col++)
-                {
-                    boardRow.Add(new Field(false, row, col));
-                }
-                chessboard.Board.Add(boardRow);
-            }
+            var chessboard = GetEmptyBoard();
 
             // White pieces
             chessboard.Board[0][4] = new Field(true, new King(true, 1000, "King"), 1, 5);
@@ -185,6 +119,23 @@ namespace Chess.Tests.SetupTests
             chessboard.Board[6][5] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 6);
             chessboard.Board[6][7] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 8);
 
+            return chessboard;
+        }
+
+        public Checkerboard GetEmptyBoard()
+        {
+            var chessboard = new Checkerboard();
+            chessboard.Board = new List<List<Field>>();
+
+            for (int row = 1; row <= 8; row++)
+            {
+                var boardRow = new List<Field>();
+                for (int col = 1; col <= 8; col++)
+                {
+                    boardRow.Add(new Field(false, row, col));
+                }
+                chessboard.Board.Add(boardRow);
+            }
             return chessboard;
         }
     }
--- Chess/Chessboard/Checkerboard.cs
+++ Chess/Chessboard/Checkerboard.cs
@@ -11,91 +11,52 @@ public class Checkerboard
 {
     public Checkerboard()
     {
-        Board = new List<List<Field>>() {
-            new List<Field>() {
-                new Field(true, new Rook(true, 5, "Rook"), 1, 1),
-                new Field(true, new Knight(true, 3, "Knight"), 1, 2),
-                new Field(true, new Bishop(true, 3, "Bishop"), 1, 3),
-                new Field(true, new Queen(true, 10, "Queen"), 1, 4),
-                new Field(true, new King(true, 1000, "King"), 1, 5),
-                new Field(true, new Bishop(true, 3, "Bishop"), 1, 6),
-                new Field(true, new Knight(true, 3, "Knight"), 1, 7),
-                new Field(true, new Rook(true, 5, "Rook"), 1, 8),
-            },
-            new List<Field>() {
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 1),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 2),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 3),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 4),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 5),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 6),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 7),
-                new Field(true, new Pawn(true, 1, "Pawn"), 2, 8),
-            },
-            new List<Field>() {
-                new Field(false, 3, 1),
-                new Field(false, 3, 2),
-                new Field(false, 3, 3),
-                new Field(false, 3, 4),
-                new Field(false, 3, 5),
-                new Field(false, 3, 6),
-                new Field(false, 3, 7),
-                new Field(false, 3, 8),
+        Board = new List<List<Field>>();
 
-            },
-            new List<Field>() {
-                new Field(false, 4, 1),
-                new Field(false, 4, 2),
-                new Field(false, 4, 3),
-                new Field(false, 4, 4),
-                new Field(false, 4, 5),
-                new Field(false, 4, 6),
-                new Field(false, 4, 7),
-                new Field(false, 4, 8),
-
-            },
-            new List<Field>() {
-                new Field(false, 5, 1),
-                new Field(false, 5, 2),
-                new Field(false, 5, 3),
-                new Field(false, 5, 4),
-                new Field(false, 5, 5),
-                new Field(false, 5, 6),
-                new Field(false, 5, 7),
-                new Field(false, 5, 8),
-            },                      
-            new List<Field>() {     
-                new Field(false, 6, 1),
-                new Field(false, 6, 2),
-                new Field(false, 6, 3),
-                new Field(false, 6, 4),
-                new Field(false, 6, 5),
-                new Field(false, 6, 6),
-                new Field(false, 6, 7),
-                new Field(false, 6, 8),
-            },
-            new List<Field>() {
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 1),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 2),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 3),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 4),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 5),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 6),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 7),
-                new Field(true, new Pawn(false, 1, "Pawn"), 7, 8),
-            },
-            new List<Field>() {
-                new Field(true, new Rook(false, 5, "Rook"), 8, 1),
-                new Field(true, new Knight(false, 3, "Knight"), 8, 2),
-                new Field(true, new Bishop(false, 3, "Bishop"), 8, 3),
-                new Field(true, new Queen(false, 10, "Queen"), 8, 4),
-                new Field(true, new King(false, 1000, "King"), 8, 5),
-                new Field(true, new Bishop(false, 3, "Bishop"), 8, 6),
-                new Field(true, new Knight(false, 3, "Knight"), 8, 7),
-                new Field(true, new Rook(false, 5, "Rook"), 8, 8),
+        for (int row = 1; row <= 8; row++)
+        {
+            var boardRow = new List<Field>();
+            for (int col = 1; col <= 8; col++)
+            {
+                boardRow.Add(new Field(false, row, col));
             }
+            Board.Add(boardRow);
+        }
+        Board[0][0] = new Field(true, new Rook(true, 5, "Rook"), 1, 1);
+        Board[0][1] = new Field(true, new Knight(true, 3, "Knight"), 1, 2);
+        Board[0][2] = new Field(true, new Bishop(true, 3, "Bishop"), 1, 3);
+        Board[0][3] = new Field(true, new Queen(true, 10, "Queen"), 1, 4);
+        Board[0][4] = new Field(true, new King(true, 1000, "King"), 1, 5);
+        Board[0][5] = new Field(true, new Bishop(true, 3, "Bishop"), 1, 6);
+        Board[0][6] = new Field(true, new Knight(true, 3, "Knight"), 1, 7);
+        Board[0][7] = new Field(true, new Rook(true, 5, "Rook"), 1, 8);
 
-        };
+        Board[1][0] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 1);
+        Board[1][1] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 2);
+        Board[1][2] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 3);
+        Board[1][3] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 4);
+        Board[1][4] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 5);
+        Board[1][5] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 6);
+        Board[1][6] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 7);
+        Board[1][7] = new Field(true, new Pawn(true, 1, "Pawn"), 2, 8);
+
+        Board[6][0] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 1);
+        Board[6][1] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 2);
+        Board[6][2] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 3);
+        Board[6][3] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 4);
+        Board[6][4] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 5);
+        Board[6][5] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 6);
+        Board[6][6] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 7);
+        Board[6][7] = new Field(true, new Pawn(false, 1, "Pawn"), 7, 8);
+
+        Board[7][0] = new Field(true, new Rook(false, 5, "Rook"), 8, 1);
+        Board[7][1] = new Field(true, new Knight(false, 3, "Knight"), 8, 2);
+        Board[7][2] = new Field(true, new Bishop(false, 3, "Bishop"), 8, 3);
+        Board[7][3] = new Field(true, new Queen(false, 10, "Queen"), 8, 4);
+        Board[7][4] = new Field(true, new King(false, 1000, "King"), 8, 5);
+        Board[7][5] = new Field(true, new Bishop(false, 3, "Bishop"), 8, 6);
+        Board[7][6] = new Field(true, new Knight(false, 3, "Knight"), 8, 7);
+        Board[7][7] = new Field(true, new Rook(false, 5, "Rook"), 8, 8);
     }
     public bool CheckIfFieldIsOutOfTheBoard(int targetRow, int targetCol)
     {
