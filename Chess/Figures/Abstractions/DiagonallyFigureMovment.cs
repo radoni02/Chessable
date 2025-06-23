d6b3c746024ab35305ea63a1b5e9eb04d9403eaf
Radosław <radekkiczekk@gmail.com>
2025-06-23 16:44:43 +0200

    fixed badly implemented diagonallFigureMovement

--- Chess/Figures/Abstractions/DiagonallyFigureMovment.cs
+++ Chess/Figures/Abstractions/DiagonallyFigureMovment.cs
@@ -11,7 +11,9 @@ namespace Chess.Figures.Abstractions
     {
         public List<Field> SelectValidFieldsOnTheDiagonals(Checkerboard checkerboard, Field currentField, int reverserRow, int reverserCol)
         {
+            var result = new MovmentResult();
             List<Field> selectedFields = new List<Field>();
+            var possibleMovesLock = false;
 
             for (int i = 1; i < 9; i++)
             {
@@ -19,13 +21,26 @@ namespace Chess.Figures.Abstractions
                     break;
                 var targetField = checkerboard.Board[(currentField.Row - 1) + (i * reverserRow)][(currentField.Col - 1) + (i * reverserCol)];
 
+                if(targetField.Figure is not null
+                                            && targetField.Figure is King
+                                            && targetField.Figure.IsWhite != currentField.Figure.IsWhite)
+                {
+                    result.AtackedFields.Add(targetField);
+                    result.PossibleMoves.Add(targetField);
+                    possibleMovesLock = true;
+                    continue;
+                }
                 if (!targetField.IsUsed)
                 {
-                    selectedFields.Add(targetField);
+                    result.AtackedFields.Add(targetField);
+                    if (!possibleMovesLock)
+                        result.PossibleMoves.Add(targetField);
+                    continue;
                 }
-                if (targetField.IsUsed && targetField.Figure.IsWhite != currentField.Figure.IsWhite)
+                if (targetField.IsUsed && targetField.Figure.IsWhite != currentField.Figure.IsWhite && !possibleMovesLock)
                 {
-                    selectedFields.Add(targetField);
+                    result.AtackedFields.Add(targetField);
+                    result.PossibleMoves.Add(targetField);
                     break;
                 }
                 if (targetField.IsUsed && targetField.Figure.IsWhite == currentField.Figure.IsWhite)
