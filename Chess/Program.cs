c91833e3d2f5d5d25ddb32b12f0a43e406e2223b
Radosław <radekkiczekk@gmail.com>
2025-06-30 17:08:52 +0200

    converted an old console application into a library and added a new demo project

--- Chess/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Chess.Chessboard;
-using Chess.Figures;
-using Chess.Utils;
-using System.Runtime.CompilerServices;
-using System.Text;
-
-var chessGame = new ChessGame();
-chessGame.StartGame();
